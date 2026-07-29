c8fb4eaf4cebbdab4cbaae781d14c6bdb992778f
Felipe Lima <67913530+falssp@users.noreply.github.com>
2026-07-29 00:39:41 -0300

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-const CACHE='terreiro-v2';
-const ASSETS=['/','/index.html','/manifest.json'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
-self.addEventListener('fetch',e=>{
-  if(e.request.url.includes('script.google.com'))return;
-  e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request).then(r=>{const cl=r.clone();caches.open(CACHE).then(ca=>ca.put(e.request,cl));return r;})).catch(()=>caches.match('/index.html')));
-});
